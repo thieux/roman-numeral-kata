b43e8085c6b8fd1292bf1e00e2df29c81f2d399f
Mathieu Pauly <mathieu.pauly@gmail.com>
2016-06-23 22:49:53 +0200

    New failing test: 1 -> "I"

---
+++ src/test/java/com/mathieupauly/romannumeralkata/RomanNumeralKataTest.java
@@ -0,0 +1,16 @@
+package com.mathieupauly.romannumeralkata;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RomanNumeralKataTest {
+    @Test
+    public void one() {
+        assertEquals("I", toRoman(1));
+    }
+
+    private String toRoman(int arabic) {
+        return null;
+    }
+}
